28f6b9b84db470859641b7d4243f92e357b82fbb
Ben Ruiz <bneruiz@me.com>
2019-11-12 17:27:34 -0700

    everything is working except about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+  
+  root 'people#index'
+
+  get '/about', to: 'people#about'
+  
+  resources :people
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
